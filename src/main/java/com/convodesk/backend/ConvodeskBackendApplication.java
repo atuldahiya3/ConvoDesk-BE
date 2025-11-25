57e2abe611e8b37cfe3736887df136bb1e82d3a3
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-11-25 18:06:55 +0530

    Setup project

---
+++ src/main/java/com/convodesk/backend/ConvodeskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.convodesk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvodeskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvodeskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/convodesk/backend/ConvodeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convodesk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvodeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
