8c2c5bd0a1e47d3340db85c12a27d897f1b8b527
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-12-02 23:14:03 +0530

    call service

---
+++ src/main/java/com/convodesk/backend/Service/TelephonyService.java
@@ -0,0 +1,10 @@
+package com.convodesk.backend.Service;
+
+import com.convodesk.backend.Entity.Business;
+import com.convodesk.backend.Entity.CallLog;
+import com.convodesk.backend.Repository.BusinessRepository;
+
+public class TelephonyService {
+
+
+}
